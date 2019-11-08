5638d6ad5835651aabd92b94fa899d1f1ebadc64
dzmitry.dzianisau@gmail.com <Dream-By>
2019-11-08 19:58:57 +0300

    Вносим изменения

--- app/src/main/java/com/example/multiactivity/AboutActivity.java
+++ app/src/main/java/com/example/multiactivity/AboutActivity.java
@@ -1,13 +1,20 @@
 package com.example.multiactivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_about);
+
+        String user = "Животное";
+        String gift = "дырку от бублика";
+
+        TextView infoTextView = (TextView)findViewById(R.id.textView_about_content);
+        infoTextView.setText(user + " , вам передали " + gift);
     }
 }
